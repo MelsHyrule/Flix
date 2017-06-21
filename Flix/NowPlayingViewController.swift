bced0a1ffd902eed27b1b1df9913def454b9e368
Melody Ann Seda Marotte <melshyrule@fb.com>
2017-06-21 15:58:10 -0700

    Table View Setup

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource  {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request =  URLRequest (url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -34,7 +39,16 @@ class NowPlayingViewController: UIViewController {
         task.resume()
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath)
+        //"MovieCell" is the identifies we gave the cell in the attributes inspector, its like a tag
+        return cell
+    }
     
     
     
